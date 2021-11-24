75099a0b6d49cd0bfd6fea7fb09bb69bcb8d9683
Oriol Bernal Martinez <youtfrost@gmail.com>
2021-11-24 09:54:24 +0100

    Tanks gets killed
    Co-Authored-By: Lucas Pérez <lucaspg14@gmail.com>

--- Tanks/Assets/Assets/Scripts/ShellScript.cs
+++ Tanks/Assets/Assets/Scripts/ShellScript.cs
@@ -19,5 +19,8 @@ public class ShellScript : MonoBehaviour
        
     }
 
-    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Tank") GameObject.Destroy(collision.gameObject);
+    }
 }
